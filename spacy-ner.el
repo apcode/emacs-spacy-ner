bb7a75ee32dc8f254b247f6216f10edbfeec11d4
Alan Patterson <alanpatterson@apcode.co.uk>
2020-01-03 22:42:57 +0000

    updated key bindings using macro def

--- spacy-ner.el
+++ spacy-ner.el
@@ -1,32 +1,43 @@
-;; spacy-mode.el -- Edit NER labels for spacy NER model
+;;; spacy-mode.el -- Edit NER labels for spacy NER model
 ;;
+;;; Commentary:
+;;
+;; Label text with NER labels for training spacy models.
+
+;;; Code:
 
 (require 'json)
 (require 'ov)
 
-
 (defvar spacy--label-properties
   '(
     (label "ORG" bg "#7B241C" char \o)
-    (label "LOCATION" bg "#943126" char \g)
-    (label "PERSON" bg "#633974" char \r)
-    (label "ACTION" bg "#1A5276" char \a)
-    (label "TASK" bg "#1A5276" char \t)
-    (label "TOOL" bg "#1A5276" char \h)
-    (label "SYSTEM" bg "#117864" char \s)
-    (label "PRODUCT" bg "#0E6655" char \p)
-    (label "PART" bg "#196F3D" char \q)
-    (label "COMPONENT" bg "#196F3D" char \c)
-    (label "INTERNAL" bg "#196F3D" char \i)
-    (label "FAULT" bg "#B9770E" char \f)
-    (label "SYMPTOM" bg "#AF601A" char \y)
+    (label "LOCATION" bg "#CD6155" char \g)
+    (label "PERSON" bg "#E74C3C" char \r)
+    (label "ACTION" bg "#FF0033" char \a)
+    (label "TASK" bg "#C2185B" char \t)
+    (label "TOOL" bg "#633974" char \h)
+    (label "SYSTEM" bg "#AB47BC" char \s)
+    (label "PRODUCT" bg "#283593" char \p)
+    (label "PART" bg "#1E88E5" char \q)
+    (label "COMPONENT" bg "#00BCD4" char \c)
+    (label "INTERNAL" bg "#00695C" char \i)
+    (label "FAULT" bg "#43A047" char \f)
+    (label "SYMPTOM" bg "#AFB42B" char \y)
     (label "MODEL" bg "#AF601A" char \e)
-    (label "MPN" bg "#AF601A" char \z)
-    (label "MATERIAL" bg "#AF601A" char \x)
-    (label "COLOR" bg "#212F3D" char \u)
-    (label "MEASURE" bg "#212F3D" char \v)
+    (label "MPN" bg "#E64A19" char \z)
+    (label "MATERIAL" bg "#795548" char \x)
+    (label "COLOR" bg "#757575" char \u)
+    (label "MEASURE" bg "#546E7A" char \v)
     )
   "Stores list of NER labels and colors.")
+
+(defun spacy--label-property (label prop)
+  "Get LABEL property PROP."
+  (plist-get (seq-find (lambda (props)
+                         (equal label (plist-get props 'label)))
+                       spacy--label-properties)
+             prop))
 
 (defun spacy-display-labels ()
   "Display overlayed labels in new buffer."
@@ -34,28 +45,74 @@
   (progn
     (pop-to-buffer
      (get-buffer-create "*-ner-tags-*"))
-    (dolist (label-prop spacy--label-properties)
-      (let ((start (point)))
-        (insert (format "%c %s" (plist-get label-prop 'char) (plist-get label-prop 'label)))
-        (ov start (point) 'face (list :background (plist-get label-prop 'bg)))
-        (newline))
-      (setq i (1+ i)))))
-
+    (erase-buffer)
+    (let ((i 1))
+      (dolist (label-prop spacy--label-properties)
+        (let ((start (point)))
+          (insert (format "%d: C-c %s %s" i (plist-get label-prop 'char) (plist-get label-prop 'label)))
+          (ov start (point) 'face (list :background (plist-get label-prop 'bg)))
+          (newline))
+        (setq i (1+ i))))))
 
 (defvar spacy-ner-mode-hook nil
   "List of functions to call when entering spacy-ner-mode.")
 
+(defvar spacy-ner-mode-map nil
+  "Keymap for spacy-ner-mode.")
+
+(defmacro spacy-define-key-label (label)
+  "Expand to interactive function for LABEL keybinding."
+  (let* ((lb label)
+         (key (format "C-c %s" (spacy--label-property lb 'char))))
+    `(define-key spacy-ner-mode-map (kbd ,key)
+                                         (lambda ()
+                                           (interactive)
+                                           (spacy-label ,lb)))))
+
+(if spacy-ner-mode-map
+    nil
+  (setq spacy-ner-mode-map (make-sparse-keymap))
+  (define-key spacy-ner-mode-map (kbd "C-c x") 'spacy-delete)
+  (define-key spacy-ner-mode-map (kbd "C-x C-s") 'spacy-save)
+  (define-key spacy-ner-mode-map (kbd "C-x C-w") 'spacy-write)
+  (define-key spacy-ner-mode-map (kbd "C-c l") 'spacy-label)
+  (define-key spacy-ner-mode-map (kbd "C-c n") 'spacy-label-n)
+  (define-key spacy-ner-mode-map (kbd "C-c m") 'spacy-label-matches)
+  (define-key spacy-ner-mode-map (kbd "C-M-<right>") 'forward-label)
+  (define-key spacy-ner-mode-map (kbd "C-M-<left>") 'backward-label)
+  (define-key spacy-ner-mode-map (kbd "C-M-S-<right>") 'forward-label-at)
+  (define-key spacy-ner-mode-map (kbd "C-M-S-<left>") 'backward-label-at)
+  ;; label specific bindings
+  ;; Can;t seem to get the following to execute in a loop,
+  ;; hence listed explicitly
+  (spacy-define-key-label "ORG")
+  (spacy-define-key-label "LOCATION")
+  (spacy-define-key-label "PERSON")
+  (spacy-define-key-label "ACTION")
+  (spacy-define-key-label "TASK")
+  (spacy-define-key-label "TOOL")
+  (spacy-define-key-label "SYSTEM")
+  (spacy-define-key-label "PRODUCT")
+  (spacy-define-key-label "PART")
+  (spacy-define-key-label "COMPONENT")
+  (spacy-define-key-label "INTERNAL")
+  (spacy-define-key-label "FAULT")
+  (spacy-define-key-label "SYMPTOM")
+  (spacy-define-key-label "MODEL")
+  (spacy-define-key-label "MPN")
+  (spacy-define-key-label "MATERIAL")
+  (spacy-define-key-label "COLOR")
+  (spacy-define-key-label "MEASURE")
+  )
 
 (defun spacy-ner ()
   "Create a spacy ner editable buffer for labelling text.
-
 Special commands:
 \\{spacy-ner-mode-map}"
   (interactive)
   (let ((ner-file (read-file-name "Enter ner training file: "))
         (inhibit-read-only t))
     (switch-to-buffer (format "*-Spacy-NER-%s-*" (file-name-base ner-file)))
-    ;;    (spacy-ner-mode)
     (erase-buffer)
     (setq major-mode 'spacy-ner-mode)
     (setq mode-name "Spacy-NER")
@@ -65,7 +122,6 @@ Special commands:
     (make-local-variable 'orig-file)
     (setq orig-file ner-file)
     (spacy--load-dataset orig-file)))
-
 
 (defun spacy--load-dataset (filename)
   "Load jsonlines from FILENAME into current buffer."
@@ -87,16 +143,14 @@ Special commands:
         (spacy--mark-line offset spans)
         (setq offset (point))))))
 
-
 (defun spacy--mark-line (offset spans)
   "Markup line at OFFSET with NER label SPANS."
   (dolist (span spans)
     (let* ((start (+ offset (gethash "start" span)))
           (end (+  offset (gethash "end" span)))
           (label (gethash "label" span))
-          (label-bg (cdr (assoc label spacy--label-properties))))
+          (label-bg (spacy--label-property label 'bg)))
       (ov start end 'face (list :background label-bg) 'label label))))
-
 
 (defun spacy--save-dataset (filename)
   "Save json dataset to FILENAME."
@@ -128,7 +182,6 @@ Special commands:
                      (list :text line :spans vec)))
             (newline)))))))
 
-
 (defalias 'forward-label 'ov-goto-next
   "Move to next label.")
 
@@ -149,12 +202,10 @@ Special commands:
     (if over
         (ov-goto-prev 'label (ov-val over 'label)))))
 
-
 (defun spacy-clear ()
   "Clear all overlays."
   (interactive)
   (ov-clear))
-
 
 (defun spacy-write ()
   "Write to new file."
@@ -164,14 +215,12 @@ Special commands:
     (setq saved-file saved-file-tmp)
     (spacy--save-dataset saved-file)))
 
-
 (defun spacy-save ()
   "Save current NER buffer back to original file."
   (interactive)
   (if (boundp 'saved-file)
       (spacy--save-dataset saved-file)
     (spacy-write)))
-
 
 (defun spacy-delete ()
   "Delete the label at point."
@@ -180,13 +229,11 @@ Special commands:
     (if over
         (ov-reset over))))
 
-
 (defun spacy-label (label)
   "Apply a LABEL to either existing or new region."
   (interactive "sLabel: ")
-  (let* ((entry (plist-get spacy--label-properties 'label))
-         (name (car entry))
-         (bg (cdr entry))
+  (let* ((name (spacy--label-property label 'label))
+         (bg (spacy--label-property label 'bg))
          (over (ov-at)))
     (if over
         (ov-set over 'face
@@ -197,12 +244,10 @@ Special commands:
           'label name)
     (deactivate-mark))))
 
-
 (defun spacy-label-n (num-label)
   "Label using NUM-LABEL entry in list."
   (interactive "nLabel Num: ")
   (spacy-label (plist-get (nth (- num-label 1) spacy--label-properties) 'label)))
-
 
 (defun spacy-label-matches ()
   "Create labels for all matching text."
@@ -212,86 +257,5 @@ Special commands:
     (ov-set text 'face (ov-val over 'face) 'label (ov-val over 'label))
     (ov-clear over)))
 
-
-(defvar spacy-ner-mode-map nil
-  "Keymap for spacy-ner-mode.")
-
-
-(if spacy-ner-mode-map
-    nil
-  (setq spacy-ner-mode-map (make-sparse-keymap))
-  (define-key spacy-ner-mode-map (kbd "C-c d") 'spacy-delete)
-  (define-key spacy-ner-mode-map (kbd "C-x C-s") 'spacy-save)
-  (define-key spacy-ner-mode-map (kbd "C-x C-w") 'spacy-write)
-  (define-key spacy-ner-mode-map (kbd "C-c l") 'spacy-label)
-  (define-key spacy-ner-mode-map (kbd "C-c n") 'spacy-label-n)
-  (define-key spacy-ner-mode-map (kbd "C-c m") 'spacy-label-matches)
-  (define-key spacy-ner-mode-map (kbd "C-M-<right>") 'forward-label)
-  (define-key spacy-ner-mode-map (kbd "C-M-<left>") 'backward-label)
-  (define-key spacy-ner-mode-map (kbd "C-M-S-<right>") 'forward-label-at)
-  (define-key spacy-ner-mode-map (kbd "C-M-S-<left>") 'backward-label-at)
-  ;; label specific bindings
-  (dolist (props spacy--label-properties)
-    (define-key spacy-ner-mode-map (kbd (format "C-c %c" (plist-get props 'char)))
-      (lambda ()
-        (interactive)
-        (spacy-label (plist-get props 'label))))
-    )
-  ;; (define-key spacy-ner-mode-map (kbd "C-c o") (lambda ()
-  ;;                                                (interactive)
-  ;;                                                (spacy-label "ORG")))
-  ;; (define-key spacy-ner-mode-map (kbd "C-c g") (lambda ()
-  ;;                                                (interactive)
-  ;;                                                (spacy-label "LOCATION")))
-  ;; (define-key spacy-ner-mode-map (kbd "C-c r") (lambda ()
-  ;;                                                (interactive)
-  ;;                                                (spacy-label "PERSON")))
-  ;; (define-key spacy-ner-mode-map (kbd "C-c a") (lambda ()
-  ;;                                                (interactive)
-  ;;                                                (spacy-label "ACTION")))
-  ;; (define-key spacy-ner-mode-map (kbd "C-c t") (lambda ()
-  ;;                                                (interactive)
-  ;;                                                (spacy-label "TASK")))
-  ;; (define-key spacy-ner-mode-map (kbd "C-c t") (lambda ()
-  ;;                                                (interactive)
-  ;;                                                (spacy-label "TOOL")))
-  ;; (define-key spacy-ner-mode-map (kbd "C-c s") (lambda ()
-  ;;                                                (interactive)
-  ;;                                                (spacy-label "SYSTEM")))
-  ;; (define-key spacy-ner-mode-map (kbd "C-c p") (lambda ()
-  ;;                                                (interactive)
-  ;;                                                (spacy-label "PRODUCT")))
-  ;; (define-key spacy-ner-mode-map (kbd "C-c r") (lambda ()
-  ;;                                                (interactive)
-  ;;                                                (spacy-label "PART")))
-  ;; (define-key spacy-ner-mode-map (kbd "C-c c") (lambda ()
-  ;;                                                (interactive)
-  ;;                                                (spacy-label "COMPONENT")))
-  ;; (define-key spacy-ner-mode-map (kbd "C-c i") (lambda ()
-  ;;                                                (interactive)
-  ;;                                                (spacy-label "INTERNAL")))
-  ;; (define-key spacy-ner-mode-map (kbd "C-c f") (lambda ()
-  ;;                                                (interactive)
-  ;;                                                (spacy-label "FAULT")))
-  ;; (define-key spacy-ner-mode-map (kbd "C-c y") (lambda ()
-  ;;                                                (interactive)
-  ;;                                                (spacy-label "SYMPTOM")))
-  ;; (define-key spacy-ner-mode-map (kbd "C-c e") (lambda ()
-  ;;                                                (interactive)
-  ;;                                                (spacy-label "MODEL")))
-  ;; (define-key spacy-ner-mode-map (kbd "C-c z") (lambda ()
-  ;;                                                (interactive)
-  ;;                                                (spacy-label "MPN")))
-  ;; (define-key spacy-ner-mode-map (kbd "C-c x") (lambda ()
-  ;;                                                (interactive)
-  ;;                                                (spacy-label "MATERIAL")))
-  ;; (define-key spacy-ner-mode-map (kbd "C-c u") (lambda ()
-  ;;                                                (interactive)
-  ;;                                                (spacy-label "COLOR")))
-  ;; (define-key spacy-ner-mode-map (kbd "C-c v") (lambda ()
-  ;;                                                (interactive)
-  ;;                                                (spacy-label "MEASURE")))
-  )
-
-
 (provide 'spacy-ner)
+;;; spacy-ner.el ends here
